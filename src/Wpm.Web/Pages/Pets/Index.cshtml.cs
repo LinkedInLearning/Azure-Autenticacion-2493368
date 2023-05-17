062b6d8cc019353c8298331949189d69cfe391de
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2023-05-17 03:27:35 -0600

    03_04f

--- src/Wpm.Web/Pages/Pets/Index.cshtml.cs
+++ src/Wpm.Web/Pages/Pets/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -6,6 +7,7 @@ using Wpm.Web.Domain;
 
 namespace Wpm.Web.Pages.Pets;
 
+[Authorize]
 public class IndexModel : PageModel
 {
     private readonly WpmDbContext dbContext;
