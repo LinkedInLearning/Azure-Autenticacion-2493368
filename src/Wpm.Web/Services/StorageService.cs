79188c79f9f01f7f4d6739d2dd7ff0d8b34f9036
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2023-05-17 00:01:51 -0600

    Inicial

---
+++ src/Wpm.Web/Services/StorageService.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Storage.Blobs;
+
+namespace Wpm.Web.Services;
+
+public class StorageService
+{
+    private readonly IConfiguration configuration;
+
+    public StorageService(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public async Task<string> UploadAsync(Stream stream, string fileName)
+    {
+        stream.Position = 0;
+        var containerClient = new
+            BlobContainerClient(configuration.GetConnectionString("WpmStorage"), "photos");
+        var blobClient = containerClient.GetBlobClient(fileName);
+        var result = await blobClient.UploadAsync(stream, true);
+        return blobClient.Uri.ToString();
+    }
+    
+}
